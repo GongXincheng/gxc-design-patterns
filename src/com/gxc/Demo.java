8116dba409c26576e5dcac6c46404214d953985e
宫新程 <gongxincheng@ruigushop.com>
2020-12-16 21:58:29 +0800

    init project

---
+++ src/com/gxc/Demo.java
@@ -0,0 +1,8 @@
+package com.gxc;
+
+/**
+ * @author GongXincheng
+ * @date 2020/12/16 21:56
+ */
+public class Demo {
+}
---
+++ src/com/gxc/singleton/SingletonDemo1.java
@@ -0,0 +1,8 @@
+package com.gxc.singleton;
+
+/**
+ * @author GongXincheng
+ * @date 2020/12/16 21:57
+ */
+public class SingletonDemo1 {
+}
