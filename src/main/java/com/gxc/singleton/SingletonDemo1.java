feb64487d11a3fd5966ee1673c547166706a3435
宫新程 <gongxincheng@ruigushop.com>
2020-12-17 21:59:19 +0800

    test reflect

--- src/main/java/com/gxc/singleton/SingletonDemo1.java
+++ src/main/java/com/gxc/singleton/SingletonDemo1.java
@@ -1,6 +1,9 @@
 package com.gxc.singleton;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.List;
 
 /**
  * 饿汉式 单例模式
@@ -9,6 +12,11 @@ import java.lang.reflect.Constructor;
  * @date 2020/12/16 21:57
  */
 public class SingletonDemo1 {
+
+    /**
+     * 用于测试泛型
+     */
+    private List<SingletonDemo1> list;
 
     private static final SingletonDemo1 instance = new SingletonDemo1();
 
@@ -21,20 +29,29 @@ public class SingletonDemo1 {
 
 }
 
-//
-//class Run {
-//
-//    public static void main(String[] args) throws Exception {
-//
-//        System.out.println(SingletonDemo1.getInstance());
-//        System.out.println(SingletonDemo1.getInstance());
-//        System.out.println(SingletonDemo1.getInstance());
-//        System.out.println(SingletonDemo1.getInstance());
-//
-//        Class<SingletonDemo1> clazz = SingletonDemo1.class;
-//        SingletonDemo1 instance = clazz.newInstance();
-//        System.out.println(instance);
-//
-//    }
-//
-//}
+
+class Run {
+
+    public static void main(String[] args) throws Exception {
+        Constructor<SingletonDemo1> constructor = SingletonDemo1.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        SingletonDemo1 singletonDemo1 = constructor.newInstance();
+
+        System.out.println(singletonDemo1);
+        System.out.println(SingletonDemo1.getInstance());
+
+        Class<SingletonDemo1> singletonDemo1Class = SingletonDemo1.class;
+
+        Field listField = singletonDemo1Class.getDeclaredField("list");
+        listField.setAccessible(true);
+
+        Class<?> type = listField.getType();
+
+        Type genericType = listField.getGenericType();
+        String typeName = genericType.getTypeName();
+        System.out.println(typeName);
+
+
+    }
+
+}
